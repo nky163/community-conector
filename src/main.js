c812512693bc87289a6c13314e0239c958666c0c
nky163 <nky8888163@gmail.com>
2022-04-20 23:06:12 +0900

    クエリをかけるように修正

--- src/main.js
+++ src/main.js
@@ -1,55 +1,71 @@
 var cc = DataStudioApp.createCommunityConnector();
 var scriptProperties = PropertiesService.getScriptProperties();
 
+var SERVICE_ACCOUNT_CREDS = 'SERVICE_ACCOUNT_CREDS';
+var SERVICE_ACCOUNT_KEY = 'private_key';
+var SERVICE_ACCOUNT_EMAIL = 'client_email';
+var BILLING_PROJECT_ID = 'project_id';
+
 function isAdminUser() {
   return true;
 }
 
 function getConfig(request) {
   var config = cc.getConfig();
-
+  
   config
-    .newInfo()
-    .setId('generalInfo')
-    .setText('This is an example connector to showcase row level security.');
+  .newTextArea()
+  .setId('query')
+  .setName('SELECT Query')
+  .setHelpText('SELECT xxx, xxx, xxx , ... FROM xxx')
+  .setPlaceholder('SELECT xxx, xxx, xxx , ... FROM xxx');
+  
+  config.setDateRangeRequired(true);
+  config.setIsSteppedConfig(false);
 
   return config.build();
 }
 
-function getFields() {
+function getFields(response) {
   var fields = cc.getFields();
   var types = cc.FieldType;
-  var aggregations = cc.AggregationType;
-
-  fields
-    .newDimension()
-    .setId('id')
-    .setName('Id')
-    .setType(types.TEXT);
-
-  fields
-    .newDimension()
-    .setId('key')
-    .setName('Key')
-    .setType(types.TEXT);
-
-  fields
-    .newDimension()
-    .setId('value')
-    .setName('Value')
-    .setType(types.TEXT);
-
+  
+  response.schema.fields.forEach(function(field) {
+    fields.newDimension()
+      .setId(field.name)
+      .setName(field.name)
+      .setType(types.TEXT);
+  });
+  
   return fields;
 }
 
 function getSchema(request) {
-  return {schema: getFields().build()};
+  
+  var serviceAccountCreds = getServiceAccountCreds();
+  var billingProjectId = serviceAccountCreds[BILLING_PROJECT_ID];
+  
+  var url = "https://bigquery.googleapis.com/bigquery/v2/projects/" + billingProjectId + "/queries";
+  
+  var response = sendRequest(url, {
+    "method": "POST",
+    "muteHttpExceptions": true,
+    "contentType": "application/json",
+    "payload": JSON.stringify({
+      "query": request.configParams.query + " LIMIT 0",
+      "useLegacySql": false
+    }),
+    "headers": { Authorization: "Bearer " + getOauthService().getAccessToken() }
+  });
+  
+  return {schema: getFields(response).build()};
 }
 
-var SERVICE_ACCOUNT_CREDS = 'SERVICE_ACCOUNT_CREDS';
-var SERVICE_ACCOUNT_KEY = 'private_key';
-var SERVICE_ACCOUNT_EMAIL = 'client_email';
-var BILLING_PROJECT_ID = 'project_id';
+function sendRequest(url, fetchOptions) {
+  return JSON.parse(UrlFetchApp.fetch(url, fetchOptions));
+}
+
+
 
 /**
  * Copy the entire credentials JSON file from creating a service account in GCP.
@@ -74,7 +90,7 @@ function getOauthService() {
   return service;
 }
 
-var BASE_SQL = 'SELECT id, key, value FROM `pristine-setup-339708.sample_dataset.sample_table`';
+// var BASE_SQL = 'SELECT id, key, value FROM `pristine-setup-339708.sample_dataset.sample_table`';
   //'SELECT d.region, d.sales, d.date ' +
   //'FROM `datastudio-solutions.row_level_security.data` d ' +
   //'INNER JOIN `datastudio-solutions.row_level_security.access` a ' +
@@ -95,7 +111,8 @@ function getData(request) {
     .setAccessToken(accessToken)
     .setBillingProjectId(billingProjectId)
     .setUseStandardSql(true)
-    .setQuery(BASE_SQL)
+    // .setQuery(BASE_SQL)
+    .setQuery(request.configParams.query)
   //  .addQueryParameter('email', bqTypes.STRING, email)
     .build();
 }
